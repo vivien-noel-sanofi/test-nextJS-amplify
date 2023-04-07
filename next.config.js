81e72d5d07350a7a0a489dfd66ed80a0763e4f53
Vivien Noel <vivien.noel@sanofi.com>
2023-04-07 09:25:19 +0200

    fix: update workflow

--- next.config.js
+++ next.config.js
@@ -11,14 +11,6 @@ const nextConfig = async () => {
 
   return {
     reactStrictMode: true,
-    i18n: {
-      // These are all the locales you want to support in
-      // your application
-      locales,
-      // This is the default locale you want to be used when visiting
-      // a non-locale prefixed path e.g. `/hello`
-      defaultLocale: locales[0],
-    },
   };
 };
 
